9a544504cc35f22aa823327fd6b18357c757968d
himanshu-varshney315 <himanshu.gupta_cs.aiml24@gla.acin>
2026-02-11 09:35:58 +0530

    Add initial project configuration files and README documentation

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,17 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define OLED_ADDR 0x3C // I2C address for the OLED display
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT);
 void setup() {
 // write your initialization code here
+
+
 }
 
 void loop() {
